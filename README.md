71eefa9d5508add7c7cfc9b30404a1bd04d05d6a
mrdog233o5 <mr.dog233o5@gmail.com>
2023-09-16 22:54:21 +0800

    added sound for mouse

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ this is a hidden folder as it starts with a dot, so you have to press `command+s
 6. when you open it, it should look like something like this:
 ```
 {
-    "pack":<>, 		#sound pack you are using
-    "volume":<>		#volume of the sound
+    "pack":<soundpack that you are using>,
+    "volume":<volume of the sound, an integer from 0 to 100>
 }
 ```
 remember to save and reopen type-sound when you made any changes
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import simpleaudio as sa
 import pathlib
 import os
-from pynput import keyboard
+from pynput import keyboard,mouse
 from os import system as osys
 
 osys("clear")
@@ -24,8 +24,10 @@ except:
     exit()
 
 try:
-    press   = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/press.wav")
+    press = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/press.wav")
     release = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/release.wav")
+    mousePress = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/mousePress.wav")
+    mouseRelease = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/mouseRelease.wav")
 except:
     print(">>> ERROR - sound pack issue, read the manual (https://github.com/mrdog233o5/type-sound)")
     exit()
@@ -46,8 +48,21 @@ def on_release(key):
         del sounds[keyboard.KeyCode.from_char(key)]
         release.play()
 
+def on_click(x, y, button, pressed):
+    global is_mouse_pressed
+    if button == mouse.Button.left:
+        if pressed:
+            is_mouse_pressed = True
+            press.play()
+        else:
+            if is_mouse_pressed:
+                is_mouse_pressed = False
+                release.play()
+
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+        on_release=on_release) as keyboard_listener:
+    with mouse.Listener(on_click=on_click) as mouse_listener:
+        keyboard_listener.join()
+        mouse_listener.join()
