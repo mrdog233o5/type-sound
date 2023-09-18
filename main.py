a064bf21958e618c39a5788c985548ed23914720
littleblack111 <littleblack11111@gmail.com>
2023-09-18 20:26:45 +0800

    updated main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/opt/homebrew/bin/python3
+#!/usr/bin/env python3
 import simpleaudio as sa
 import pathlib
 import os
@@ -25,7 +25,7 @@ except:
     print(">>> ERROR - config file error, read the manual (https://github.com/mrdog233o5/type-sound)")
     exit()
 
-try
+try:
     press = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/press.wav")
     release = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/release.wav")
     mousePress = sa.WaveObject.from_wave_file(f"{localaddr}/sounds/{soundPack}/mousePress.wav")
