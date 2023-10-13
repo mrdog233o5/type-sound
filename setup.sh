df2b0ee6fb1f7ae55a078458c79e7797a23efb36
mrdog233o5 <mr.dog233o5@gmail.com>
2023-10-13 21:47:01 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -9,9 +9,3 @@ sudo install ./dist/type-sound /usr/local/bin
 echo "Using sudo to install to /Applications"
 # install -d ./dist/type-sound.app /Applications
 sudo cp -r ./dist/type-sound.app /Applications
-sudo cp -r ./dist/type-sound.app /Applications
-sudo mkdir /opt/user
-sudo mkdir /opt/user/bin
-sudo cp -r ./dist/type-sound /opt/user/bin
-cd ~
-echo 'export PATH="/opt/user/bin":$PATH' >> .zshrc
